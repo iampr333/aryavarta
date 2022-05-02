f4b78466af5432c2a6e29c5e559534c1f9e97119
Prash <iampr333@gmail.com>
2022-05-02 18:48:55 +0530

    up

--- lib/card_container_style.dart
+++ lib/card_container_style.dart
@@ -1,37 +1,43 @@
+import 'package:aryavarta/Screens/medieval_content.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:aryavarta/Constant.dart';
-double offset=5;
-double shadowBlur=0.0;
+
+double offset = 5;
+double shadowBlur = 0.0;
+
 class CardContainer extends StatelessWidget {
   String cardTitle;
   String imgLink;
-  Function onTap;
-  CardContainer(this.cardTitle, this.imgLink,this.onTap);
+  CardContainer(this.cardTitle, this.imgLink);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap:() {
-        onTap;
+      onTap: () {
+        if (cardTitle == 'Medieval history') {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => MedivalContent(),
+            ),
+          );
+        }
       },
       child: Card(
         margin: EdgeInsets.all(8),
         elevation: 15,
-        color:Color(0xFF333366),
-
+        color: Color(0xFF333366),
         child: Column(
           children: [
             Expanded(
-              flex: 4,
+              flex: 8,
               child: Container(
-
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.vertical(
                       top: Radius.circular(5),
                       bottom: Radius.zero,
                     ),
-
                     image: DecorationImage(
                       image: NetworkImage(
                         imgLink,
@@ -40,7 +46,6 @@ class CardContainer extends StatelessWidget {
                     )),
               ),
             ),
-
             const SizedBox(
               height: 2,
             ),
@@ -48,7 +53,7 @@ class CardContainer extends StatelessWidget {
               flex: 1,
               child: Text(
                 cardTitle,
-                textAlign: TextAlign.center,
+                //  textAlign: TextAlign.center,
                 style: kTitleTextStyle,
               ),
             ),
