9bebdc24b92f07e4b95ecaf4317e64e121f92634
saran <saranshb964@gmail.com>
2022-05-03 16:21:22 +0530

    done some data for medeval history

---
+++ lib/data/mideval history/mideval_data.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/cupertino.dart';
+
+class MedievalData {
+  List northIndiaDynasty = [
+    Dynasty(
+        dynastyName: "Slave Dynasty",
+        // link map of every dynasty
+        era: Pair(1206, 1290),
+        about: "",
+        kings: [
+          Kings(kingsName: "Qutubdin Aibak",
+              about: "",
+              era: Pair(1206,1211),
+              images: []),
+          Kings(kingsName: "Aram Shah",
+              about: "",
+              era: Pair(1210,1211),
+              images: []),
+          Kings(kingsName: "Iltutmish",
+              about: "",
+              era: Pair(1211,1236),
+              images: []),
+          Kings(kingsName: "Ruknuddin",
+              about: "",
+              era: Pair(1236,1236),
+              images: []),
+          Kings(kingsName: "Rajiya Sultan",
+              about: "",
+              era: Pair(1236,1240),
+              images: []),
+          Kings(kingsName: "MUiz ud din Bahram",
+              about: "",
+              era: Pair(1240,1242),
+              images: []),
+          Kings(kingsName: "Alaudin Masud",
+              about: "",
+              era: Pair(1242,1246),
+              images: []),
+          Kings(kingsName: "Nasiruddin Mahmud",
+              about: "",
+              era: Pair(1246,1266),
+              images : []),
+          Kings(kingsName: "Ghiasuddin Balban",
+              about: "",
+              era: Pair(1266,1286),
+              images: []),
+          Kings(kingsName: "Muzidin Qaiqabad",
+              about: "",
+              era: Pair(1286,1290),
+              images: []),
+        ]
+    ),
+    Dynasty(dynastyName: "Khilji Dynasty", 
+        era: Pair(1290,1320),
+        about: "",
+        kings: [
+          Kings(kingsName: "Jalaluddin Khilji" ,
+              about: "",
+              era: Pair(1290,1296),
+              images: []),
+          Kings(kingsName: "Alaudding Khilji",
+              about: "",
+              era: Pair(1296,1316),
+              images: []),
+          Kings(kingsName: "Umar Khan Khilji",
+              about: "",
+              era: Pair(1316,1316),
+              images: []),
+          Kings(kingsName: "Qutub ud din Mubarak Shah",
+              about: "",
+              era: Pair(1316,1320),
+              images: []),
+          Kings(kingsName: "Khusro Khan",
+              about: "",
+              era: Pair(1320,1320),
+              images: []),
+        ]),
+    Dynasty(dynastyName: "Tughlqus",
+        era: Pair(1320,1412),
+        about: "",
+        kings: [
+          Kings(kingsName: "Ghiyas-ud-din Tughluq",
+              about: "",
+              era: Pair(1320,1324),
+              images: []),
+          Kings(kingsName: "Muhammad bin Tughluq",
+              about: "",
+              era: Pair(1325,1351),
+              images: []),
+          Kings(kingsName: "Firoz Shah Tughluq",
+              about: "",
+              era: Pair(1351,1388),
+              images: []),
+          Kings(kingsName: "Abu Bakar Shah",
+              about: "",
+              era: Pair(1389,1390),
+              images: []),
+          Kings(kingsName: "Sultan Nasir-ud-din Tughluq",
+              about: "",
+              era: Pair(1394,1413),
+              images: [])
+        ]),
+    Dynasty(dynastyName: "Sayyid Dynasty",
+        era: Pair(1414,1451),
+        about: "Sauuid Dunastyu was funded by Khizr Khan , a former governer of Multan , they succeeded the Tughlaq dynasty",
+        kings: [
+          Kings(kingsName: "Khizr Khan",
+              about:"",
+
+              era: Pair(1414,1421),
+              images: []),
+          Kings(kingsName: "Mubarak Shah",
+              about:"",
+              era: Pair(1421,1434),
+              images: []),
+          Kings(kingsName: "Muhammad Shah",
+              about:"",
+              era: Pair(1434,1445),
+              images: []),
+          Kings(kingsName: "Alam Shah",
+              about:"",
+              era: Pair(1445,1451),
+              images: [])
+        ]),
+    Dynasty(dynastyName: "Lodi's Dynasty",
+        about:"",
+        era: Pair(1451,1526),
+
+        kings: [
+          Kings(kingsName: "bahlol Lodi",
+              about:"",
+
+              era: Pair(1451,1489),
+              images: []),
+          Kings(kingsName: "Sikandar Lodi",
+              about:"",
+              era: Pair(1489,1517),
+              images: []),
+          Kings(kingsName: "Ibraham Lodi",
+              about:"",
+              era: Pair(1517,1526),
+              images: [])
+        ]),
+    Dynasty(dynastyName: "Mughals",
+        era: Pair(1526,1707),
+        about: "",
+        kings: [
+          Kings(kingsName: "Babar",
+              about:"",
+              era: Pair(1526,1530),
+              images: []),
+          Kings(kingsName: "Humayun",
+              about:"",
+              era:  Pair(1530,1556),
+              images: []),
+          Kings(kingsName: "Akbar",
+              about:"",
+              era: Pair(1556,1605),
+              images: []),
+          Kings(kingsName: "Jahangir",
+              about:"",
+              era: Pair(1605,1627),
+              images: []),
+          Kings(kingsName: "Shah Jahan",
+              about:"",
+              era: Pair(1628,1657),
+              images: []),
+          Kings(kingsName: "Aurangzeb",
+              about:"",
+              era: Pair(1658,1707),
+              images: [])
+        ]),
+    Dynasty(dynastyName: "later Mughal",
+        era: Pair(1707,1757),
+        about: "after Aurangzeb, the decline of Mughals was initiated. After Aurangzeb, there are problem of foreigners invasion and ruler are weak",
+        kings: [
+          Kings(kingsName: "Bahadur Shah ",
+              about:"",
+              era: Pair(1707,1712),
+              images: []),
+          Kings(kingsName: "Jahandar Shah",
+              about:"",
+              era: Pair(1712,1713),
+              images: []),
+          Kings(kingsName: "Farrkuhsiyar",
+              about:"",
+              era: Pair(1713,1718),
+              images: []),
+          Kings(kingsName: "Mahammad Shah",
+              about:"",
+              era: Pair(1719,1748),
+              images: []),//Nadir Shah Atack
+          Kings(kingsName: "Ahmad Shah",
+              about:"",
+              era: Pair(1748,1754), 
+              images: []),
+          Kings(kingsName: "Alamgir 2",
+              about:"",
+              era: Pair(1754,1759),
+              images:[] ),
+          Kings(kingsName: "Shah Alam",
+              about:"",
+              era: Pair(1759,1806),
+              images: []),
+          Kings(kingsName: "Akbar 2",
+              about:"",
+              era: Pair(1806,1837),
+              images: []),
+          Kings(kingsName: "Bahadur Shah",
+              about:"",
+              era: Pair(1837,1857),
+              images: [])
+        ]),
+  ];
+
+  List southIndiaDynasty = [
+    Dynasty(dynastyName: "Sangama Dynasty/Vijaynagar Empire",
+        era: Pair(1336,1356),
+        about: "",
+        kings: [
+          Kings(kingsName: "Harihar Raya 1",
+              about:"",
+              era: Pair(1336,1356),
+              images: []),
+          Kings(kingsName: "Bukkka Raya 1",
+              about:"",
+              era: Pair(1356,1377) ,
+              images: []),
+          Kings(kingsName: "Harihar Raya 2",
+              about:"",
+              era: Pair(1377,1404),
+              images: []),
+          Kings(kingsName: "Virupakha Raya",
+              about:"",
+              era: Pair(1404,1405),
+              images:[]),
+          Kings(kingsName: "Bukka Raya 2",
+              about:"",
+              era: Pair(1405,1406),
+              images: []),
+          Kings(kingsName: "Deva Raya 1",
+              about:"",
+              era: Pair(1406,1422),
+              images: []),
+          Kings(kingsName: "Ramachandra Raya",
+              about:"",
+              era: Pair(1422,1422),
+              images: []),
+          Kings(kingsName: "Deva Raya 2",
+              about:"",
+              era: Pair(1424,1446),
+              images:[]),
+          Kings(kingsName: "Mallikarjun Raya",
+              about:"",
+              era: Pair(1446,1465),
+              images: []),
+          Kings(kingsName: "Virupaksha Raya 2",
+              about:"",
+              era: Pair(1465,1485),
+              images: []),
+          Kings(kingsName: "Praudha Raya",
+              about:"",
+              era: Pair(1485,1485),
+              images: [])
+        ])
+  ];
+
+}
+
+class Dynasty{
+  String dynastyName;
+  Pair era;
+  String about;
+  List<Kings> kings ;
+  List<Events> events = [];
+  MindMap mindmap = MindMap();
+  Maps map = Maps();
+  Dynasty({required this.dynastyName, required this.era, required this.about, required this.kings});
+}
+class Kings {
+  String kingsName;
+  Pair era;
+  String about;
+  List<Events> events = [];
+  Maps map = Maps();
+  List<Image> images;
+  Kings({required this.kingsName, required this.era, required this.images ,required this.about});
+}
+
+class Pair{
+  var first;
+  var second;
+  Pair(this.first,this.second);
+}
+class Maps{
+  String asset = "";
+
+}
+class MindMap{
+  String asset = "";
+}
+class Image{
+  late String image;
+  late String notes;
+}
+
+class Events{
+  var date;
+  String detail;
+  Image image ;
+
+  Events({this.date, required this.detail, required this.image});
+}
+
+
+
+
